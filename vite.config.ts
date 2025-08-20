34819428ec057d41518f117463ee22546ed39970
SybioXyandex <sybio1@yandex.com>
2025-08-20 23:07:03 +0300

    Update vite.config.ts
    Signed-off-by: SybioXyandex <sybio1@yandex.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Bu basit kurulum için özel bir yapılandırma gerekmemektedir.
-  define: {
-    // Ortam değişkenini istemci tarafı kodunda kullanılabilir hale getirir.
-    // DİKKAT: Bu API anahtarını doğrudan koda eklemek güvenlik riski oluşturabilir.
-    // Üretim ortamında bu anahtarı bir ortam değişkeni (environment variable) olarak ayarlamanız önerilir.
-    'process.env.API_KEY': JSON.stringify('AIzaSyA5eAy7oc1zZmJYo10oWWb1mqShHVmWbHM')
-  }
 })
