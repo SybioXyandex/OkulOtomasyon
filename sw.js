ac2c5163441728e2a4cbabcdb9cfb456827bd1d4
SybioXyandex <sybio1@yandex.com>
2025-08-20 16:26:23 +0300

    Update sw.js
    Signed-off-by: SybioXyandex <sybio1@yandex.com>

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'okul-otomasyon-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/index.css',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './index.css',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
