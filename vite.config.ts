30c6fa3f890a374b5a381326b53b97261baa0394
SybioXyandex <sybio1@yandex.com>
2025-08-25 13:53:09 +0300

    Update vite.config.ts
    Signed-off-by: SybioXyandex <sybio1@yandex.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import process from 'process'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // Bu basit kurulum için özel bir yapılandırma gerekmemektedir.
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // vite config
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  }
 })
